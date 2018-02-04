7b86920e2ba79eb62cd89ffebeea878a52d8e2f0
Dan Vega <danvega@gmail.com>
2018-02-04 07:52:14 -0500

    Setting up relationship between track and student and adding some test data.

--- src/main/java/com/therealdanvega/tech/domain/Student.java
+++ src/main/java/com/therealdanvega/tech/domain/Student.java
@@ -1,12 +1,9 @@
 package com.therealdanvega.tech.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Data
